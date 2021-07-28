5cf6a6bf004cc4101cc6e17116880b779c3dd982
Tuanaoz <88100761+Tuanaoz@users.noreply.github.com>
2021-07-28 20:35:48 +0300

    Create isPalindrome.java

---
+++ isPalindrome.java
@@ -0,0 +1,12 @@
+public boolean isPalindrome(String s) {
+        int j = s.length()-1;  
+        int i = 0; 
+        while(i<(s.length()/2 + 1)){
+            if(s.charAt(i) != s.charAt(j)){
+                return false;
+            }
+            i++;
+            j--;
+        }
+        return true;
+    }
